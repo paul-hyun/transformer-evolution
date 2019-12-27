8e29a8fc65ab724a13e9e28a992811084d6aeebb
Ubuntu <ubuntu@cchyun.novalocal>
2019-12-27 16:59:47 +0900

    change comment for shape

--- gpt/model.py
+++ gpt/model.py
@@ -117,7 +117,7 @@ class DecoderLayer(nn.Module):
         # (bs, n_dec_seq, d_hidn)
         ffn_outputs = self.pos_ffn(self_att_outputs)
         ffn_outputs = self.layer_norm3(self_att_outputs + ffn_outputs)
-        # (bs, n_dec_seq, d_hidn), (bs, n_head, n_dec_seq, n_dec_seq), (bs, n_head, n_dec_seq, n_enc_seq)
+        # (bs, n_dec_seq, d_hidn), (bs, n_head, n_dec_seq, n_dec_seq)
         return ffn_outputs, self_attn_prob
 
 
@@ -149,10 +149,10 @@ class Decoder(nn.Module):
 
         self_attn_probs = []
         for layer in self.layers:
-            # (bs, n_dec_seq, d_hidn), (bs, n_dec_seq, n_dec_seq), (bs, n_dec_seq, n_enc_seq)
+            # (bs, n_dec_seq, d_hidn), (bs, n_dec_seq, n_dec_seq)
             dec_outputs, self_attn_prob = layer(dec_outputs, dec_self_attn_mask)
             self_attn_probs.append(self_attn_prob)
-        # (bs, n_dec_seq, d_hidn), [(bs, n_dec_seq, n_dec_seq)], [(bs, n_dec_seq, n_enc_seq)]S
+        # (bs, n_dec_seq, d_hidn), [(bs, n_dec_seq, n_dec_seq)]
         return dec_outputs, self_attn_probs
 
 
